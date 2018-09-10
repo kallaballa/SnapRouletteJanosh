131b14b1b409722c25b07d05aa5ae73860d1dc58
kallaballa <amir@viel-zu.org>
2018-09-10 08:35:40 -0400

    updated roulette.lua

--- roulette.lua
+++ roulette.lua
@@ -5,18 +5,16 @@ Janosh:mkarr("/roulette/.")
 
 function receive(handle, message)
     if message == 'setup' then
-      Janosh:wsBroadcast("{}")
+--      Janosh:wsBroadcast("{}")
     else
       local obj = JSON:decode(message)
       if obj[2] == "submit" then
         local s = Janosh:size("/roulette/.")
-	if s == 0 then
-		return	
+	if s > 0 then
+		local i = math.random(s - 1)
+        	Janosh:wsSend(handle,Janosh:raw("/roulette/#" .. i))
 	end
-	local i = math.random(s - 1)
-        Janosh:publish(handle,"clear", "W", "")
-        Janosh:wsSend(handle,Janosh:raw("/roulette/#" .. i))
-        Janosh:append_t("/roulette/..", obj[4]);
+        Janosh:append_t("/roulette/.", obj[4])
       end
     end
 end
